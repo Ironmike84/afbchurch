1b0679e0fb30e08ed724f93efdb94368bbacdbd1
ironmike84 <michaelgross.tn@gmail.com>
2022-05-22 17:43:39 -0400

    New

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import { Button } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
 function NavBar() {
   return (
-    <div>
-      <Link to={'/Home'}><Button>Home</Button></Link>
-      <Link to={'/Services'}><Button>Services</Button></Link>
-      <Link to={'/Children'}><Button>Children</Button></Link>
+    <div className='NavBar'>
+      <Link to={'/Home'}><Button className="btn-dark">Home</Button></Link>
+      <Link to={'/Services'}><Button className="btn-dark">Services</Button></Link>
+      <Link to={'/Children'}><Button className="btn-dark">Children</Button></Link>
     </div>
   )
 }
